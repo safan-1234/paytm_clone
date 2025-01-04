0f0d4d165a4cb50d37f86edbd026f263de3acd24
safan-1234 <safwanbegar786@gmail.com>
2025-01-04 17:02:41 +0530

    Update server.js

--- paytm-app/backend/server.js
+++ paytm-app/backend/server.js
@@ -7,17 +7,16 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
-// Define routes
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
